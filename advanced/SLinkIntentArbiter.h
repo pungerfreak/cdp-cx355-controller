f8982e2b0c431e3d214fa183cf1f94b5915b30d8
Daniel Greenlaw <daniel.e.greenlaw@gmail.com>
2026-01-10 13:43:04 -0800

    Enhance intent arbitration policies

--- advanced/SLinkIntentArbiter.h
+++ advanced/SLinkIntentArbiter.h
@@ -1,15 +1,25 @@
 #pragma once
 #include <Arduino.h>
 #include "SLinkIntents.h"
+#include "SLinkIntentQueue.h"
 
 class SLinkIntentArbiter {
 public:
   explicit SLinkIntentArbiter(uint32_t dedupeWindowMs = 250);
 
   bool shouldDispatch(const SLinkCommandIntent& intent);
+  bool selectNext(SLinkIntentQueue& queue, SLinkCommandIntent& intent);
 
 private:
   static constexpr uint8_t kIntentTypeCount = 7;
+
+  struct Policy {
+    uint8_t priority;
+    uint32_t throttleMs;
+    uint32_t expireMs;
+  };
+
+  static const Policy& policyFor(SLinkIntentType type);
 
   uint32_t _lastDispatchMs[kIntentTypeCount] = {};
   uint32_t _dedupeWindowMs;
--- advanced/SLinkIntentQueue.cpp
+++ advanced/SLinkIntentQueue.cpp
@@ -3,6 +3,7 @@
 bool SLinkIntentQueue::push(const SLinkCommandIntent& intent) {
   if (isFull()) return false;
   _queue[_tail] = intent;
+  _queuedAt[_tail] = millis();
   _tail = (uint8_t)((_tail + 1) % kMaxIntents);
   _count++;
   return true;
@@ -16,10 +17,39 @@ bool SLinkIntentQueue::pop(SLinkCommandIntent& intent) {
   return true;
 }
 
+bool SLinkIntentQueue::peekAt(uint8_t offset,
+                              SLinkCommandIntent& intent,
+                              uint32_t& enqueuedAt) const {
+  if (offset >= _count) return false;
+  uint8_t index = (uint8_t)((_head + offset) % kMaxIntents);
+  intent = _queue[index];
+  enqueuedAt = _queuedAt[index];
+  return true;
+}
+
+bool SLinkIntentQueue::removeAt(uint8_t offset,
+                                SLinkCommandIntent& intent,
+                                uint32_t& enqueuedAt) {
+  if (!peekAt(offset, intent, enqueuedAt)) return false;
+  for (uint8_t i = offset; i + 1 < _count; ++i) {
+    uint8_t fromIndex = (uint8_t)((_head + i + 1) % kMaxIntents);
+    uint8_t toIndex = (uint8_t)((_head + i) % kMaxIntents);
+    _queue[toIndex] = _queue[fromIndex];
+    _queuedAt[toIndex] = _queuedAt[fromIndex];
+  }
+  _tail = (uint8_t)((_tail + kMaxIntents - 1) % kMaxIntents);
+  _count--;
+  return true;
+}
+
 bool SLinkIntentQueue::isEmpty() const {
   return _count == 0;
 }
 
 bool SLinkIntentQueue::isFull() const {
   return _count >= kMaxIntents;
+}
+
+uint8_t SLinkIntentQueue::size() const {
+  return _count;
 }
